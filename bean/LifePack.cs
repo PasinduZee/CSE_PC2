fa452e835704ee2e932c9fcba17c7f1397195c81
unknown <zeeleo7@gmail.com>
2015-10-16 22:17:18 +0530

    First commit

---
+++ bean/LifePack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mustank.bean
+{   
+    //Coicider merging with the parent
+    class LifePack : MapItem
+    {
+        public LifePack(int x, int y): base(x, y)
+        {
+
+        }
+    }
+}
